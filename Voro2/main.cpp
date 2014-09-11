643b26a75084e8cd687d4a3a6d23e5fe5995367f
Caglar Sinayuc <caglars@pc-241-9.wls.metu.edu.tr>
2014-09-11 15:39:47 +0300

    Initial Commit

---
+++ Voro2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Voro2
+//
+//  Created by Caglar Sinayuc on 11/09/14.
+//  Copyright (c) 2014 caglars. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
